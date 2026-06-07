3764ab6bbc2a811eb9c4cb45cd45f93024a3a2d2
Jay <jaynikhar61@gmail.com>
2026-06-07 16:12:03 +0530

    Clean up authRoutes by removing commented code
    Removed commented-out code for unused routes.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,13 +6,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 
-// export default router;
-// import express from "express";
-// import { registerUser } from "../controllers/authController.js";
-
-// const router = express.Router();
-
-// router.post("/register", registerUser);
 
 
 export default router;
