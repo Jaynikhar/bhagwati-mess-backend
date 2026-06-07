ed0eb59bee2844f2b6b7fcdcbfe86403fbdbd20b
Jay <jaynikhar61@gmail.com>
2026-06-07 16:13:32 +0530

    Clean up User.js by removing commented code
    Removed commented-out userSchema code from User.js

--- src/models/User.js
+++ src/models/User.js
@@ -18,14 +18,3 @@ const userSchema = new mongoose.Schema(
 
 export default mongoose.model("User", userSchema);
 
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     name: { type: String, required: true },
-//     email: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-//   },
-//   { timestamps: true }
-// );
-
-// export default mongoose.model("User", userSchema);
